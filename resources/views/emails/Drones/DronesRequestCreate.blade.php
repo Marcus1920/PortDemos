d19e471b4bf21187bdc63820190f618f9fc24786
Hyfos2 <bthighforce@gmail.com>
2017-12-06 13:24:21 +0200

    update

--- resources/views/emails/Drones/DronesRequestCreate.blade.php
+++ resources/views/emails/Drones/DronesRequestCreate.blade.php
@@ -76,7 +76,7 @@
                                 <td width="100%" height="15"></td>
                             </tr>
                             <tr>
-                                <td width="100%" style="font-family:helvetica, Arial, sans-serif; padding-left: 20px; font-size: 13px; text-align: left; color:#2E363F; line-height: 24px;">
+                                <td width="100%" style="font-family:helvetica, Arial, sans-serif; padding-left: 20px; font-size: 13px; text-align: center; color:#2E363F; line-height: 24px;">
                                    <a href="{{ env('LIVE_URL') }}"  class="button" style=" background-color: #F55125;padding: 6px 9px;display: inline-block;text-decoration: none;color: #FFFFFF;border-radius: 1px;"><strong>Click here to login</strong></a>
 
                                 </td>
@@ -84,6 +84,8 @@
 
                             <tr>
                                 <td width="100%" style="font-family:helvetica, Arial, sans-serif; font-size: 14px; text-align: left; color:#87919F; line-height: 24px;">
+                                    <p>Kindly Regards</p>
+                                    <p>Siyaleader Investments.</p>
 
                                 </td>
                             </tr>
@@ -112,20 +114,17 @@
                 <tbody>
                 <tr>
                     <td style="border-collapse:collapse; vertical-align:middle; text-align center; padding:20px;">
-
                         <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
                             <tbody>
                             <tr><!-- logo -->
                                 <td width="100%" style="font-family: helvetica, Arial, sans-serif; font-size: 18px; letter-spacing: 0px; text-align: center;">
                                     <a href="#" style="text-decoration: none;">
-                                        <img src="{{ $message->embed('assets/img/siyaleader_light_bg_100.png') }}"  width='90px' alt="Siyaleader"/>
-                                        {{--<img src="{{ asset('img/siyaleader_light_bg_100.png') }}" width='90px' alt="Siyaleader" />--}}
+                                        <img src="{{ asset('img/siyaleader_light_bg_100.png') }}" width='90px' alt="Siyaleader" />
                                     </a>
                                 </td>
                             </tr>
                             </tbody>
                         </table>
-
                     </td>
                 </tr>
                 </tbody>
